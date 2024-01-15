9d01a839857e62f1d34f6747b2a5152646dc6aae
TheSerperiorOne <77816253+TheSerperiorOne@users.noreply.github.com>
2024-01-15 08:36:11 -0800

    Adding main files (to run things easier)

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Varun Thvar on 1/15/24.
+//
+
+#include "main.h"
+#include "iostream"
+using namespace std;
+
+int main()
+{
+   cout << "Hello World!";
+}
---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by Varun Thvar on 1/15/24.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
