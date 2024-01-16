7fc4351ccc8273f28877260a89d7457d93f3ccdc
TheSerperiorOne <77816253+TheSerperiorOne@users.noreply.github.com>
2024-01-16 15:56:51 -0800

    Adding main files (to run things easier)

--- main.cpp
+++ main.cpp
@@ -8,5 +8,13 @@ using namespace std;
 
 int main()
 {
+   int* numbers = new int[3];
+   *(numbers+4) = 4;
+
+   for (int i = 0; i < 5; i++)
+   {
+      cout << numbers[i] << endl;
+   }
+
    cout << "Hello World!";
 }
