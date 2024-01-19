83eeb80b06e3f752b311b654698431f61737a984
TheSerperiorOne <77816253+TheSerperiorOne@users.noreply.github.com>
2024-01-18 17:53:38 -0800

    worked on excel model

--- main.cpp
+++ main.cpp
@@ -9,12 +9,14 @@ using namespace std;
 int main()
 {
    int* numbers = new int[3];
+   cout << sizeof(numbers) << endl;
    *(numbers+4) = 4;
 
    for (int i = 0; i < 5; i++)
    {
       cout << numbers[i] << endl;
    }
+   cout << sizeof(numbers) << endl;
 
    cout << "Hello World!";
 }
