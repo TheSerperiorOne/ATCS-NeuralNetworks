21a68e921e9d575bef53d7c8fe62b57772ff8547
Varun Thvar <25varunt@students.harker.org>
2024-01-26 21:34:17 -0800

    Fixed Small Issues

---
+++ cmake-build-debug/CMakeFiles/ATCS_Neural_Networks.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "ATCS_Neural_Networks.exe"
+  "ATCS_Neural_Networks.exe.manifest"
+  "ATCS_Neural_Networks.pdb"
+  "CMakeFiles/ATCS_Neural_Networks.dir/AB1Network/AB1Network.cpp.obj"
+  "CMakeFiles/ATCS_Neural_Networks.dir/AB1Network/AB1Network.cpp.obj.d"
+  "CMakeFiles/ATCS_Neural_Networks.dir/main.cpp.obj"
+  "CMakeFiles/ATCS_Neural_Networks.dir/main.cpp.obj.d"
+  "libATCS_Neural_Networks.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ATCS_Neural_Networks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ATCS_Neural_Networks.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ATCS_Neural_Networks.
+# This may be replaced when dependencies are built.
